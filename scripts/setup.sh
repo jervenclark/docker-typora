aede9d06074b2ad54d42641b93bc5fbab5b80851
Jerven Clark Chua <jervenclark@users.noreply.github.com>
2020-06-03 11:00:30 +0800

    Initial commit

---
+++ scripts/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+name=''
+desc=''
+
+while getopts ":n:d:" opt; do
+    case $opt in
+        n) name=$(sed -E "s/(\s+)/-/g;s/(.*)/\L\1/" <<< "$OPTARG")
+           ;;
+        d) desc="$OPTARG"
+           ;;
+        \?) echo "Invalid option -$OPTARG" >&2
+            ;;
+    esac
+done
+
+if [ -n "$name" ];
+then
+    ln -fs $PWD/git-hooks/pre-commit .git/hooks/pre-commit
+    sed -E "s/template/$name/;s/some description/$desc/" Dockerfile
+fi
